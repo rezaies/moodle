5b9d498ab4b773ee1e0946e8057a145175f99b09
Shamim Rezaie <shamim@moodle.com>
2020-01-10 18:59:58 +1100

    MDL-69166 pg_paypal: Ability to connect to sandbox or live environment

--- payment/gateway/paypal/classes/external.php
+++ payment/gateway/paypal/classes/external.php
@@ -149,12 +149,14 @@ class external extends external_api {
 
         $config = get_config('pg_paypal');
 
+        $sandbox = $config->environment == 'sandbox';
+
         [
             'amount' => $amount,
             'currency' => $currency
         ] = \core_payment\helper::get_cost($component, $componentid);
 
-        $paypalhelper = new paypal_helper($config->clientid, $config->secret, false);
+        $paypalhelper = new paypal_helper($config->clientid, $config->secret, $sandbox);
         $authorization = $paypalhelper->capture_authorization($authorizationid, $amount, $currency);
 
         $success = false;
