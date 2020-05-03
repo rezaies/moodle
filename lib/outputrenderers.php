aed7190ddfec39b7026d95ebaa701ec94bd83b76
Shamim Rezaie <shamim@moodle.com>
2020-05-03 16:04:05 +1000

    MDL-68076 core: display the feedback link on footer

--- lib/outputrenderers.php
+++ lib/outputrenderers.php
@@ -835,6 +835,12 @@ class core_renderer extends renderer_base {
             }
         }
 
+        $output .= html_writer::div(
+                $this->render_from_template('core/userfeedback_footer_link', []),
+                'userfeedback_footer_placeholder'
+        );
+        $this->page->requires->js_call_amd('core/userfeedback', 'registerFeedbackLink', ['.userfeedback_footer_placeholder']);
+
         // This function is normally called from a layout.php file in {@link core_renderer::header()}
         // but some of the content won't be known until later, so we return a placeholder
         // for now. This will be replaced with the real content in {@link core_renderer::footer()}.
