b1fc4d2f78d6a6b25d9a9b8b89efd4aadb68de39
Shamim Rezaie <shamim@moodle.com>
2020-09-30 13:24:43 +1000

    MDL-69166 enrol_fee: Show a warning when no currency is supported

--- enrol/fee/settings.php
+++ enrol/fee/settings.php
@@ -26,43 +26,51 @@ defined('MOODLE_INTERNAL') || die();
 
 if ($ADMIN->fulltree) {
 
-    $settings->add(new admin_setting_heading('enrol_fee_settings', '', get_string('pluginname_desc', 'enrol_fee')));
+    $currencies = enrol_get_plugin('fee')->get_currencies();
 
-    // Note: let's reuse the ext sync constants and strings here, internally it is very similar,
-    // it describes what should happen when users are not supposed to be enrolled any more.
-    $options = array(
-        ENROL_EXT_REMOVED_KEEP           => get_string('extremovedkeep', 'enrol'),
-        ENROL_EXT_REMOVED_SUSPENDNOROLES => get_string('extremovedsuspendnoroles', 'enrol'),
-        ENROL_EXT_REMOVED_UNENROL        => get_string('extremovedunenrol', 'enrol'),
-    );
-    $settings->add(new admin_setting_configselect(
+    if (empty($currencies)) {
+        $notify = new \core\output\notification(get_string('nocurrencysupported', 'core_payment'),
+            \core\output\notification::NOTIFY_WARNING);
+        $settings->add(new admin_setting_heading('enrol_fee_nocurrency', '', $OUTPUT->render($notify)));
+        $settings->add(new admin_setting_heading('enrol_fee_settings', '', get_string('pluginname_desc', 'enrol_fee')));
+    } else {
+        $settings->add(new admin_setting_heading('enrol_fee_settings', '', get_string('pluginname_desc', 'enrol_fee')));
+
+        // Note: let's reuse the ext sync constants and strings here, internally it is very similar,
+        // it describes what should happen when users are not supposed to be enrolled any more.
+        $options = array(
+            ENROL_EXT_REMOVED_KEEP           => get_string('extremovedkeep', 'enrol'),
+            ENROL_EXT_REMOVED_SUSPENDNOROLES => get_string('extremovedsuspendnoroles', 'enrol'),
+            ENROL_EXT_REMOVED_UNENROL        => get_string('extremovedunenrol', 'enrol'),
+        );
+        $settings->add(new admin_setting_configselect(
             'enrol_fee/expiredaction',
             get_string('expiredaction', 'enrol_fee'),
             get_string('expiredaction_help', 'enrol_fee'),
             ENROL_EXT_REMOVED_SUSPENDNOROLES,
             $options));
 
-    $settings->add(new admin_setting_heading('enrol_fee_defaults',
-        get_string('enrolinstancedefaults', 'admin'), get_string('enrolinstancedefaults_desc', 'admin')));
+        $settings->add(new admin_setting_heading('enrol_fee_defaults',
+            get_string('enrolinstancedefaults', 'admin'), get_string('enrolinstancedefaults_desc', 'admin')));
 
-    $options = array(ENROL_INSTANCE_ENABLED  => get_string('yes'),
-                     ENROL_INSTANCE_DISABLED => get_string('no'));
-    $settings->add(new admin_setting_configselect('enrol_fee/status',
-        get_string('status', 'enrol_fee'), get_string('status_desc', 'enrol_fee'), ENROL_INSTANCE_DISABLED, $options));
+        $options = array(ENROL_INSTANCE_ENABLED  => get_string('yes'),
+                         ENROL_INSTANCE_DISABLED => get_string('no'));
+        $settings->add(new admin_setting_configselect('enrol_fee/status',
+            get_string('status', 'enrol_fee'), get_string('status_desc', 'enrol_fee'), ENROL_INSTANCE_DISABLED, $options));
 
-    $settings->add(new admin_setting_configtext('enrol_fee/cost', get_string('cost', 'enrol_fee'), '', 0, PARAM_FLOAT, 4));
+        if (!during_initial_install()) {
+            $settings->add(new admin_setting_configtext('enrol_fee/cost', get_string('cost', 'enrol_fee'), '', 0, PARAM_FLOAT, 4));
+            $settings->add(new admin_setting_configselect('enrol_fee/currency', get_string('currency', 'enrol_fee'), '', '',
+                $currencies));
 
-    $currencies = enrol_get_plugin('fee')->get_currencies();
-    $settings->add(new admin_setting_configselect('enrol_fee/currency', get_string('currency', 'enrol_fee'), '', '', $currencies));
+            $options = get_default_enrol_roles(context_system::instance());
+            $student = get_archetype_roles('student');
+            $student = reset($student);
+            $settings->add(new admin_setting_configselect('enrol_fee/roleid',
+                get_string('defaultrole', 'enrol_fee'), get_string('defaultrole_desc', 'enrol_fee'), $student->id, $options));
+        }
 
-    if (!during_initial_install()) {
-        $options = get_default_enrol_roles(context_system::instance());
-        $student = get_archetype_roles('student');
-        $student = reset($student);
-        $settings->add(new admin_setting_configselect('enrol_fee/roleid',
-            get_string('defaultrole', 'enrol_fee'), get_string('defaultrole_desc', 'enrol_fee'), $student->id, $options));
+        $settings->add(new admin_setting_configduration('enrol_fee/enrolperiod',
+            get_string('enrolperiod', 'enrol_fee'), get_string('enrolperiod_desc', 'enrol_fee'), 0));
     }
-
-    $settings->add(new admin_setting_configduration('enrol_fee/enrolperiod',
-        get_string('enrolperiod', 'enrol_fee'), get_string('enrolperiod_desc', 'enrol_fee'), 0));
 }
