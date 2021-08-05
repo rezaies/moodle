81a5beac5d7c70d0f7b2b46dd451b9ea6814960d
Shamim Rezaie <shamim@moodle.com>
2021-08-05 17:15:49 +1000

    MDL-70721 core: a new mustache helper to clean string after get_string

--- lib/outputrenderers.php
+++ lib/outputrenderers.php
@@ -102,6 +102,7 @@ class renderer_base {
 
             $loader = new \core\output\mustache_filesystem_loader();
             $stringhelper = new \core\output\mustache_string_helper();
+            $cleanstringhelper = new \core\output\mustache_clean_string_helper();
             $quotehelper = new \core\output\mustache_quote_helper();
             $jshelper = new \core\output\mustache_javascript_helper($this->page);
             $pixhelper = new \core\output\mustache_pix_helper($this);
@@ -113,6 +114,7 @@ class renderer_base {
 
             $helpers = array('config' => $safeconfig,
                              'str' => array($stringhelper, 'str'),
+                             'strs' => array($cleanstringhelper, 'strs'),
                              'quote' => array($quotehelper, 'quote'),
                              'js' => array($jshelper, 'help'),
                              'pix' => array($pixhelper, 'pix'),
