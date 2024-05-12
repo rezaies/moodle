d5871e480a14a439165ac414a9f062c9ac89ef4e
Shamim Rezaie <shamim@moodle.com>
2024-05-13 04:29:06 +1000

    fixup! MDL-80599 enrol_guest: Support password update in csv course upload

--- enrol/guest/lib.php
+++ enrol/guest/lib.php
@@ -553,21 +553,15 @@ class enrol_guest_plugin extends enrol_plugin {
      * @return array Errors
      */
     public function validate_enrol_plugin_data(array $enrolmentdata, ?int $courseid = null): array {
-        global $CFG;
-
         // If password is omitted or empty in csv it will be generated automatically if it is a required policy.
 
         $errors = parent::validate_enrol_plugin_data($enrolmentdata, $courseid);
 
         $policy = $this->get_config('usepasswordpolicy');
         if (!empty($enrolmentdata['password']) && $policy) {
-            $errmsg = '';
-            $errarray = [];
-            if (!check_password_policy($enrolmentdata['password'], $errmsg, null, $errarray)) {
-                foreach ($errarray as $err) {
-                    $passwordconfig = str_replace('error', '', $err);
-                    $errors[$err] = new lang_string($err, 'auth', $CFG->$passwordconfig);
-                }
+            $errarray = get_password_policy_errors($enrolmentdata['password']);
+            foreach ($errarray as $i => $err) {
+                $errors['enrol_guest' . $i] = $err;
             }
         }
         return $errors;
